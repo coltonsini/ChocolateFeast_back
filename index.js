201da306ca28a59ab5f554e868d1d12d8682ff90
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2022-06-22 19:59:54 -0500

    Changes
    update

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const casos = [
+    {id: 1, n: 10, c: 2, m:5, enroll: true},
+    {id: 2, n: 10, c: 2, m:5, enroll: false},
+    {id: 3, n: 10, c: 2, m:5, enroll: false}
+];
+
+app.get('/', (req, res) => {   
+    res.send('Chocolate Feast Api');
+});
+
+app.get('/api/casos', (req, res) => {
+    res.send(casos);
+});
+    
+
+app.get('/api/casos/:id', (req, res) => {
+    const caso = casos.find(c => c.id === parseInt(req.params.id));
+    if (!caso) return res.status(404).send('Caso no encontrado');
+    else res.send(caso);
+})
+
+app.post('/api/casos', (req,res) => {
+    const caso = {
+        id: casos.length + 1,
+        n: req.body.n,
+        c: req.body.c,
+        m: req.body.m,
+        enroll: (req.body.enroll == 'true')
+    };
+
+    casos.push(caso);
+    res.send(caso);
+})
+
+app.delete('/api/casos/:id', (req, res) => {
+    const caso = casos.find(c => c.id === parseInt(req.params.id));
+    if(!caso) return res.status(404).send('Caso no encontrado');
+
+    const index = casos.indexOf(caso);
+    casos.splice(index, 1);
+    res.send(caso);
+})
+
+const port = process.env.port || 80;
+app.listen(port, () => console.log(`Escuchando el puerto ${port}`));
