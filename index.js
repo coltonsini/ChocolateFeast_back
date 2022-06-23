48637c6f70fead8ece25fef1411868c96a20af6b
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2022-06-22 21:04:52 -0500

    cors access
    changes

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const app = express();
 
 app.use(express.json());
 
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 const casos = [
     {id: 1, n: 10, c: 2, m:5, enroll: true},
     {id: 2, n: 10, c: 2, m:5, enroll: false},
